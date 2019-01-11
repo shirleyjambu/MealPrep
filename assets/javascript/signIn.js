4848dde3e21cb9f73686ad1c2d8ba691fc465ae0
Elbe <ashelbe08@gmail.com>
2019-01-11 11:22:48 -0500

    Added login

---
+++ assets/javascript/signIn.js
@@ -0,0 +1,50 @@
+
+
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyDu6OahnJpzNLQbl3aFDi_WssS6-uFPXGw",
+  authDomain: "mealprep-6dc6e.firebaseapp.com",
+  databaseURL: "https://mealprep-6dc6e.firebaseio.com",
+  projectId: "mealprep-6dc6e",
+  storageBucket: "mealprep-6dc6e.appspot.com",
+  messagingSenderId: "1028879243201"
+};
+firebase.initializeApp(config);
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'welcome.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: 'welcome.html',
+  // Privacy policy url.
+ // privacyPolicyUrl: 'trains.html'
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
