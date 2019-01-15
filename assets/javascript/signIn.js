4a3c5888ec9e95a7b517bcf2746fd05956ed8864
da Vinci <brucewilson1103@gmail.com>
2019-01-15 10:05:47 -0500

    rando

--- assets/javascript/signIn.js
+++ assets/javascript/signIn.js
@@ -17,10 +17,11 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      console.log(authResult)
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      return true;
+      // return true;
     },
     uiShown: function() {
       // The widget is rendered.
